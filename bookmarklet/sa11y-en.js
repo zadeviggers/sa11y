cd52dbb76a597079f41ac93cba3f1ec214785f99
Zade Viggers <74938858+zadeviggers@users.noreply.github.com>
2023-04-14 21:49:19 +1200

    Update sa11y-en.js

--- bookmarklet/sa11y-en.js
+++ bookmarklet/sa11y-en.js
@@ -9,19 +9,20 @@
 ------------------------------------------------------------------------*/
 
 const version = "latest"; //Version
+const username = "zadeviggers";
 
 if (typeof Sa11y !== "object") {
     /* Append sa11y.css */
     const sa11ycss = document.createElement("link");
     sa11ycss.setAttribute("rel", "stylesheet");
-    sa11ycss.setAttribute("href", `https://cdn.jsdelivr.net/gh/zadeviggers/sa11y@${version}/dist/css/sa11y.min.css`);
+    sa11ycss.setAttribute("href", `https://cdn.jsdelivr.net/gh/${username}/sa11y@${version}/dist/css/sa11y.min.css`);
     sa11ycss.setAttribute("type", "text/css");
 
     const bodyheader = document.getElementsByTagName("head")[0];
     bodyheader.appendChild(sa11ycss);
 
     const combine = document.createElement("script");
-    combine.src = `https://cdn.jsdelivr.net/combine/gh/ryersondmp/sa11y@${version}/dist/js/lang/en.umd.js,gh/ryersondmp/sa11y@${version}/dist/js/sa11y.umd.min.js`;
+    combine.src = `https://cdn.jsdelivr.net/combine/gh/${uusername}/sa11y@${version}/dist/js/lang/en.umd.js,gh/${username}/sa11y@${version}/dist/js/sa11y.umd.min.js`;
 
     document.body.appendChild(combine);
     combine.onload = combine.onreadystatechange = function() {
